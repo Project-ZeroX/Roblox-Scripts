6de0af7f0047a152b347238aac03dc865060fe60
JackMercy <project.zerodatax@gmail.com>
2024-09-28 11:44:20 +0700

    Update VersionChecker.lua

--- Verified/VersionChecker.lua
+++ Verified/VersionChecker.lua
@@ -1,7 +1,7 @@
 getgenv().ProjectZeroX_Version = {
     ["Game Lists"] = {
-        [16146832113] = "https://pastebin.com/raw/wrNPQzKa",
-        [16277809958] = "https://pastebin.com/raw/wrNPQzKa",
-        [18219125606] = "https://pastebin.com/raw/wrNPQzKa",
+        [16146832113] = "https://raw.githubusercontent.com/Project-ZeroX/Roblox-Scripts/refs/heads/main/Verified/Version_Checker/Anime_Vanguards.lua",
+        [16277809958] = "https://raw.githubusercontent.com/Project-ZeroX/Roblox-Scripts/refs/heads/main/Verified/Version_Checker/Anime_Vanguards.lua",
+        [18219125606] = "https://raw.githubusercontent.com/Project-ZeroX/Roblox-Scripts/refs/heads/main/Verified/Version_Checker/Anime_Vanguards.lua",
     }
 }
