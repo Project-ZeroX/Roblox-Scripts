1994778203c4c25a2f33238423bff4154d19a245
JackMercy <project.zerodatax@gmail.com>
2024-09-28 11:40:24 +0700

    Create VersionChecker.lua

---
+++ Verified/VersionChecker.lua
@@ -0,0 +1,7 @@
+getgenv().ProjectZeroX_Version = {
+    ["Game Lists"] = {
+        [16146832113] = "https://pastebin.com/raw/wrNPQzKa",
+        [16277809958] = "https://pastebin.com/raw/wrNPQzKa",
+        [18219125606] = "https://pastebin.com/raw/wrNPQzKa",
+    }
+}
