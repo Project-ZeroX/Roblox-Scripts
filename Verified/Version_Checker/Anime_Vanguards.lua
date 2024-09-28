f23e543cd7986f6c8e4d85f3b3598979e270f87f
JackMercy <project.zerodatax@gmail.com>
2024-09-28 11:43:45 +0700

    Create Anime_Vanguards.lua

---
+++ Verified/Version_Checker/Anime_Vanguards.lua
@@ -0,0 +1,7 @@
+local _env = getgenv and getgenv() or {}
+
+local Script_Version = "1.0"
+local Script_Discord = "https://discord.gg/projectzerox"
+
+_env.Version = Script_Version
+_env.Discord = Script_Discord
